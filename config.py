a1214abe9fd64a6f8375c47e0c71a84b5dc792c1
Ryan Sommers <ryansommers@gmail.com>
2016-12-21 22:37:25 -0500

    Added league averages to config

--- config.py
+++ config.py
@@ -2,4 +2,16 @@
 #dict of league avg event probabilities
 #individual player attributes modify these? 
 
-LEAGUE_AVG = {}
+LG_AVG = {
+	'1b' : .14919
+	'2b' : .04472
+	'3b' : .00472
+	'hr' : .03039
+	'bb' : .08174
+	'so' : .21120
+	'hbp' : .00894
+	'gdp' : .02015
+	'sb' : .01374
+	'csRate' : .39456
+}
+
