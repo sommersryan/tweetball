59bc51d8c48b807cc2f72a95f6d97c9d825a8836
Ryan Sommers <ryansommers@gmail.com>
2016-12-22 20:40:45 -0500

    Simplified attributes with kwargs

--- attributes.py
+++ attributes.py
@@ -2,54 +2,41 @@ import const
 
 class Hitting(object):
 	
-	def __init__(self, singles = const.singles.mean, doubles = const.doubles.mean,
-			triples = const.triples.mean, homers = const.homers.mean, strikeouts = const.strikeouts.mean, 
-			walks = const.walks.mean, hbp = const.hbp.mean, inPlayOuts = const.inPlayOuts.mean,
-			steals = const.steals.mean, csRate = const.csRate.mean):
+	def __init__(self, **kwargs):
 		
-		#Initializes an attribute set of an average hitter
+		for key, value in kwargs.items():
+			setattr(self, key, value)
 		
-		self.singles = singles
-		self.doubles = doubles
-		self.triples = triples
-		self.homers = homers
-		self.strikeouts = strikeouts
-		self.walks = walks
-		self.hbp = hbp
-		self.inPlayOuts = inPlayOuts
-		self.steals = steals
-		self.csRate = csRate
-		
-		allHits = singles + doubles + triples + homers
+		allHits = kwargs['singles'] + kwargs['doubles'] + kwargs['triples'] + kwargs['homers']
 		leagueHits = const.singles.mean + const.doubles.mean + const.triples.mean + const.homers.mean
 		
-		self.contact = round(((allHits/strikeouts)/(leagueHits/const.strikeouts.mean))*5)
+		self.contact = round(((allHits/kwargs['strikeouts'])/(leagueHits/const.strikeouts.mean))*5)
 		
-		xbh = doubles + triples + homers
+		xbh = kwargs['doubles'] + kwargs['triples'] + kwargs['homers']
 		leagueXbh = const.doubles.mean + const.triples.mean + const.homers.mean
 		
-		self.power = round(((xbh/singles)/(leagueXbh/const.singles.mean))*5)
-		self.discipline = round(((walks/(strikeouts+inPlayOuts))/(const.walks.mean/(const.strikeouts.mean+const.inPlayOuts.mean)))*5)
-		self.speed = round((steals/const.steals.mean)*5)
+		self.power = round(((xbh/kwargs['singles'])/(leagueXbh/const.singles.mean))*5)
+		self.discipline = round(((kwargs['walks']/(kwargs['strikeouts']+kwargs['inPlayOuts']))/(const.walks.mean/(const.strikeouts.mean+const.inPlayOuts.mean)))*5)
+		self.speed = round((kwargs['steals']/const.steals.mean)*5)
 		
 	@classmethod
 	def random(cls):
 	
 		#Creates a random hitting skillset
-		
-		singles = const.singles.generateAttribute()
-		doubles = const.doubles.generateAttribute()
-		triples = const.triples.generateAttribute()
-		homers = const.homers.generateAttribute()
-		strikeouts = const.strikeouts.generateAttribute()
-		walks = const.walks.generateAttribute()
-		hbp = const.hbp.generateAttribute()
-		inPlayOuts = const.inPlayOuts.generateAttribute()
-		steals = const.steals.generateAttribute()
-		csRate = const.csRate.generateAttribute()
-		
-		inst = cls(singles, doubles, triples, homers, strikeouts, walks,
-				hbp, inPlayOuts, steals, csRate)
+		attrs = {
+			'singles' : const.singles.generateAttribute(),
+			'doubles' : const.doubles.generateAttribute(),
+			'triples' : const.triples.generateAttribute(),
+			'homers' : const.homers.generateAttribute(),
+			'strikeouts' : const.strikeouts.generateAttribute(),
+			'walks' : const.walks.generateAttribute(),
+			'hbp' : const.hbp.generateAttribute(),
+			'inPlayOuts' : const.inPlayOuts.generateAttribute(),
+			'steals' : const.steals.generateAttribute(),
+			'csRate' : const.csRate.generateAttribute()
+			}
+			
+		inst = cls(**attrs)
 				
 		return inst
 		
