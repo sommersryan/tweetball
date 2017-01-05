e3a770339cd2050982fb16a41bfbd0a9bfeacf2d
Ryan Sommers <ryansommers@gmail.com>
2017-01-05 01:05:14 -0500

    Added standard deviations to league environment

--- league.py
+++ league.py
@@ -4,57 +4,68 @@ AVG_OUTCOMES = {
 					'HR' : {
 								'hi' : 70,
 								'lo' : 1,
-								'mean' : 27
+								'mean' : 27,
+								'sd' : 20
 							},
 					'triple' : {
 								'hi' : 32,
 								'lo' : 1,
-								'mean' : 5
+								'mean' : 5,
+								'sd' : 5 
 							},
 					'double' : {
 								'hi' : 81,
 								'lo' : 11,
-								'mean' : 45
+								'mean' : 45,
+								'sd' : 11
 							},
 					'single' : {
 								'hi' : 260,
 								'lo' : 80,
-								'mean' : 152
+								'mean' : 152,
+								'sd' : 30
 							},
 					'BB' : {
 								'hi' : 206,
 								'lo' : 20,
-								'mean' : 78
+								'mean' : 78,
+								'sd' : 30
 							},
 					'HBP' : {
 								'hi' : 58,
 								'mean' : 9,
-								'lo' : 1
+								'lo' : 1,
+								'sd' : 7
 							},
 					'inPlayOut' : {
 									'hi' : 585,
 									'mean' : 442,
-									'lo' : 299
+									'lo' : 299,
+									'sd' : 56
 								},
 					'strikeout' : {
 									'hi' : 360,
 									'mean' : 206,
-									'lo' : 70
+									'lo' : 70,
+									'sd' : 60
 								},
 					'sacrifice' : {
 									'hi' : 45,
 									'mean' : 6,
-									'lo' : 0
+									'lo' : 0,
+									'sd' : 5
 								},
 					'GDP' : {
 								'hi' : 45,
 								'mean' : 20,
-								'lo' : 1
+								'lo' : 1,
+								'sd' : 9
 							},
 					'error' : {
 								'hi' : 10,
 								'mean' : 10,
-								'lo' : 10
+								'lo' : 10,
+								'sd' : 1
 							}
 				}
 				
