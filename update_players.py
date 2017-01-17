15937dedfc77b1b5d98bc4d643eee1b38cf39ef6
Ryan Sommers <ryansommers@gmail.com>
2017-01-17 11:40:18 -0500

    Completed player list update script

--- update_players.py
+++ update_players.py
@@ -1,7 +1,7 @@
 from tweet import api
 from tweepy import Cursor
 from storage import playerStore
-import time
+import time, roster
 
 IDs = []
 
@@ -14,3 +14,11 @@ currentPlayers = [int(a.key) for a in list(playerStore.list())]
 addList = list(set(IDs) - set(currentPlayers))
 removeList = list(set(currentPlayers) - set(IDs))
 
+for addition in addList:
+
+	player = roster.Player(api.get_user(addition))
+	player.save()
+	
+for removal in removeList:
+
+	playerStore.delete_key(removal)
