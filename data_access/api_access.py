253a51612ea5754a35f8de0e7fc56503184de158
Ryan Sommers <rsommers@s3gov.com>
2018-11-07 11:27:50 -0500

    Upgrading requests

--- data_access/api_access.py
+++ data_access/api_access.py
@@ -2,7 +2,7 @@ import requests
 import os
 
 auth = (os.getenv('API_USER'), os.getenv('API_PASSWORD'))
-base_url = "http://{0}/".format(os.getenv('API_IP')
+base_url = "http://{0}/".format(os.getenv('API_IP'))
 
 
 
