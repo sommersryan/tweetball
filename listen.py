61888e3906e7ab9bdd230dd78d18862fae8fefd3
Ryan Sommers <ryansommers@gmail.com>
2021-11-09 11:03:42 -0500

    publishing game result

--- listen.py
+++ listen.py
@@ -3,27 +3,26 @@ from run import execute_from_json
 from config import AMQP_URL
 
 connection = pika.BlockingConnection(
-    parameters=pika.URLParameters(AMQP_URL)
+	parameters=pika.URLParameters(AMQP_URL)
 )
 
-channel = connection.channel()
-
-channel.exchange_declare(exchange='games', exchange_type='topic')
-
-result = channel.queue_declare('', exclusive=True)
-
+sub_channel = connection.channel()
+sub_channel.exchange_declare(exchange='games', exchange_type='topic')
+result = sub_channel.queue_declare('', exclusive=True)
 queue_name = result.method.queue
+sub_channel.queue_bind(exchange='games', queue=queue_name, routing_key='games.execution.start')
 
-channel.queue_bind(exchange='games', queue=queue_name, routing_key='games.execution.start')
+pub_channel = connection.channel()
+pub_channel.exchange_declare(exchange='games', exchange_type='topic')
 
 
 def callback(ch, method, properties, body):
-    game_result_json = execute_from_json(body)
-    print(game_result_json)
+	game_result_json = execute_from_json(body)
+	pub_channel.basic_publish(exchange='games', routing_key='games.execution.complete', body=game_result_json)
 
 
-channel.basic_consume(
-    queue=queue_name, on_message_callback=callback, auto_ack=True
+sub_channel.basic_consume(
+	queue=queue_name, on_message_callback=callback, auto_ack=True
 )
 
-channel.start_consuming()
+sub_channel.start_consuming()
